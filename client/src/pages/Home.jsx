86570a1e64675c2a82aaa2d8824b4c4ec1a498ab
dstorie80 <dstorie80@gmail.com>
2024-05-02 18:56:34 -0500

    updated the landing page and created a new header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,36 +1,16 @@
-import { useQuery } from '@apollo/client';
+import React from "react";
+import "../App.css";
+import Navbar from "../components/Header/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import ThoughtList from '../components/ThoughtList';
-import ThoughtForm from '../components/ThoughtForm';
-
-import { QUERY_THOUGHTS } from '../utils/queries';
-
-const Home = () => {
-  const { loading, data } = useQuery(QUERY_THOUGHTS);
-  const thoughts = data?.thoughts || [];
-
+function Home() {
   return (
-    <main>
-      <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          <ThoughtForm />
-        </div>
-        <div className="col-12 col-md-8 mb-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ThoughtList
-              thoughts={thoughts}
-              title="Some Feed for Thought(s)..."
-            />
-          )}
-        </div>
+    <>
+      <div className="navContainer">
+        <Navbar />
       </div>
-    </main>
+    </>
   );
-};
+}
 
 export default Home;
