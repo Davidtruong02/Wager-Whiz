8ece082277b2c748cd68a89eb4909b39e45a082b
David Truong <Davidtruong134@gmail.com>
2024-05-05 14:53:33 -0500

    edited player card text spacing

--- client/src/components/Cards/BasicCard.jsx
+++ client/src/components/Cards/BasicCard.jsx
@@ -39,20 +39,20 @@ function BasicCard({
       {imageUrl && <Card.Img variant="top" src={imageUrl} />}
       <Card.Body>
         <Card.Title>{playerName}</Card.Title>
-        <Card.Text>
-          {sport && `Sport: ${sport}`}
-          {category && `Category: ${category}`}
-          {line && `Line: ${line}`}
-          {typeOfLine && `Type of Line: ${typeOfLine}`}
-          {position && `Position: ${position}`}
-          {team && `Team: ${team}`}
-          {opponent && `Opponent: ${opponent}`}
-          {usagePercent && `Usage Percent: ${usagePercent}`}
-          {minutes && `Minutes: ${minutes}`}
-          {minutesPercentage && `Minutes Percentage: ${minutesPercentage}`}
-          {projection && `Projection: ${projection}`}
-          {dvaPositionDefense && `DVA Position Defense: ${dvaPositionDefense}`}
-        </Card.Text>
+  
+      {sport && <p className="mb-2">Sport: {sport}</p>}
+      {category && <p className="mb-2">Category: {category}</p>}
+      {line && <p className="mb-2">Line: {line}</p>}
+      {typeOfLine && <p className="mb-2">Type of Line: {typeOfLine}</p>}
+      {position && <p className="mb-2">Position: {position}</p>}
+      {team && <p className="mb-2">Team: {team}</p>}
+      {opponent && <p className="mb-2">Opponent: {opponent}</p>}
+      {usagePercent && <p className="mb-2">Usage Percent: {usagePercent}</p>}
+      {minutes && <p className="mb-2">Minutes: {minutes}</p>}
+      {minutesPercentage && <p className="mb-2">Minutes Percentage: {minutesPercentage}</p>}
+      {projection && <p className="mb-2">Projection: {projection}</p>}
+      {dvaPositionDefense && <p className="mb-2">DVA Position Defense: {dvaPositionDefense}</p>}
+
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
