3c2e2fc03c07f7ba3a7ec3c74501a010caf2403a
dstorie80 <dstorie80@gmail.com>
2024-05-07 14:50:38 -0500

    Error was with bootstrap css

--- client/src/components/Cards/BasicCard.jsx
+++ client/src/components/Cards/BasicCard.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import "./BasicCard.css";
 
 function BasicCard({
   playerName,
   sport,
-  team,
-  opponent,
   category,
   line,
   typeOfLine,
   position,
+  team,
+  opponent,
   usagePercent,
   minutes,
   minutesPercentage,
@@ -25,14 +25,13 @@ function BasicCard({
           {imageUrl && <Card.Img variant="top" src={imageUrl} />}
           <Card.Body>
             <Card.Title>{playerName}</Card.Title>
-            {sport && <p className="mb-2">Sport: {sport}</p>}
             {team && <p className="mb-2">Team: {team}</p>}
             {opponent && <p className="mb-2">Opponent: {opponent}</p>}
           </Card.Body>
         </Card>
-
         <Card className="card-back" style={{ width: "18rem" }}>
           <Card.Body>
+            {sport && <p className="mb-2">Sport: {sport}</p>}
             {category && <p className="mb-2">Category: {category}</p>}
             {line && <p className="mb-2">Line: {line}</p>}
             {typeOfLine && <p className="mb-2">Type of Line: {typeOfLine}</p>}
@@ -48,6 +47,7 @@ function BasicCard({
             {dvaPositionDefense && (
               <p className="mb-2">DVA Position Defense: {dvaPositionDefense}</p>
             )}
+            <Button variant="primary">Add to picks</Button>
           </Card.Body>
         </Card>
       </div>
