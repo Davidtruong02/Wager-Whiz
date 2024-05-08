4ca6029e3b33bd7887866ccaedb3cfe8a47c1acf
dstorie80 <dstorie80@gmail.com>
2024-05-07 19:33:47 -0500

    Put the cards into a container with tabs

--- client/src/components/Forms/CardPanel.jsx
+++ client/src/components/Forms/CardPanel.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import BasicCard from "../Cards/BasicCard";
-import "../../App.css";
+import { Tab, Nav, Row, Col } from "react-bootstrap";
 
 function CardPanel({ selectedSport }) {
   const [playerData, setPlayerData] = useState([]);
@@ -19,27 +19,72 @@ function CardPanel({ selectedSport }) {
   }, [selectedSport]);
 
   return (
-    <div className="playerCards">
-      {playerData.map((player) => (
-        <BasicCard
-          key={player._id}
-          playerName={player.playerName}
-          sport={player.sport}
-          category={player.category}
-          line={player.line}
-          typeOfLine={player.typeOfLine}
-          position={player.position}
-          team={player.team}
-          opponent={player.opponent}
-          usagePercent={player.usagePercent}
-          minutes={player.minutes}
-          minutesPercentage={player.minutesPercentage}
-          projection={player.projection}
-          dvaPositionDefense={player.dvaPositionDefense}
-          imageUrl={player.imageUrl}
-        />
-      ))}
-    </div>
+    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+      <Row>
+        <Col sm={12}>
+          <Nav variant="pills" className="flex-row">
+            <Nav.Item>
+              <Nav.Link eventKey="first">Prize Picks</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="second">Underdog</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={12}>
+          <Tab.Content>
+            <Tab.Pane eventKey="first">
+              <div className="playerCards">
+                {playerData.slice(0, playerData.length / 2).map((player) => (
+                  <BasicCard
+                    key={player._id}
+                    playerName={player.playerName}
+                    sport={player.sport}
+                    category={player.category}
+                    line={player.line}
+                    typeOfLine={player.typeOfLine}
+                    position={player.position}
+                    team={player.team}
+                    opponent={player.opponent}
+                    usagePercent={player.usagePercent}
+                    minutes={player.minutes}
+                    minutesPercentage={player.minutesPercentage}
+                    projection={player.projection}
+                    dvaPositionDefense={player.dvaPositionDefense}
+                    imageUrl={player.imageUrl}
+                  />
+                ))}
+              </div>
+            </Tab.Pane>
+            <Tab.Pane eventKey="second">
+              <div className="playerCards">
+                {playerData.slice(playerData.length / 2).map((player) => (
+                  <BasicCard
+                    key={player._id}
+                    playerName={player.playerName}
+                    sport={player.sport}
+                    category={player.category}
+                    line={player.line}
+                    typeOfLine={player.typeOfLine}
+                    position={player.position}
+                    team={player.team}
+                    opponent={player.opponent}
+                    usagePercent={player.usagePercent}
+                    minutes={player.minutes}
+                    minutesPercentage={player.minutesPercentage}
+                    projection={player.projection}
+                    dvaPositionDefense={player.dvaPositionDefense}
+                    imageUrl={player.imageUrl}
+                  />
+                ))}
+              </div>
+            </Tab.Pane>
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
   );
 }
 
