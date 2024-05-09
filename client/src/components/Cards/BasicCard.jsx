a3391768c5f279e0291be4379381596e25299d47
dstorie80 <dstorie80@gmail.com>
2024-05-08 22:32:17 -0500

    Updated the cards so they are smaller and better data

--- client/src/components/Cards/BasicCard.jsx
+++ client/src/components/Cards/BasicCard.jsx
@@ -34,39 +34,95 @@ function BasicCard({
       });
   }, [sport, team]);
 
+  const calculateScore = () => {
+    if (line) {
+      return ((projection / line) * 100 - 100).toFixed(2);
+    }
+    return 0;
+  };
+
+  const score = calculateScore();
+
   return (
-    <div className="card-container">
+    <div
+      className="card-container"
+      style={{ marginBottom: "-175px", minHeight: "100%" }}
+    >
       <div className="card-body">
         <Card
           className="card-front"
-          style={{ width: "18rem", backgroundImage: `url(${logoUrl})` }}
+          style={{
+            width: "18rem",
+            maxHeight: "60%",
+            backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url(https://a.espncdn.com/i/teamlogos/${sport}/500/${team}.png)`,
+            backgroundSize: "contain",
+          }}
         >
-          {imageUrl && <Card.Img variant="top" src={imageUrl} />}
+          {imageUrl && (
+            <Card.Img
+              style={{
+                maxHeight: "40%",
+                maxWidth: "40%",
+                display: "block",
+                marginLeft: "auto",
+                marginRight: "auto",
+              }}
+              variant="top"
+              src={imageUrl}
+            />
+          )}
           <Card.Body>
-            <Card.Title>{playerName}</Card.Title>
-            {team && <p className="mb-2">Team: {team}</p>}
+            <Card.Title style={{ fontSize: "15px", fontWeight: "bold" }}>
+              {team && `${team} - `}
+              {position}
+            </Card.Title>
+            {playerName}
             {opponent && <p className="mb-2">Opponent: {opponent}</p>}
+            {projection && line && (
+              <p
+                style={{
+                  color: score < 0 ? "red" : "green",
+                  fontWeight: "bolder",
+                  // textShadow: "2px 2px #000",
+                }}
+                className="mb-2"
+              >
+                Proj: {projection} [{score}%]
+              </p>
+            )}
+            {category && line && (
+              <p className="mb-2">
+                Prop: {line} {category}
+              </p>
+            )}
+            {/* {line && <p className="mb-2">Line: {line}</p>} */}
           </Card.Body>
         </Card>
-        <Card className="card-back" style={{ width: "18rem" }}>
+        <Card
+          className="card-back"
+          style={{
+            width: "18rem",
+            maxHeight: "60%",
+            backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url(https://a.espncdn.com/i/teamlogos/nba/500/${team}.png)`,
+            backgroundSize: "contain",
+          }}
+        >
           <Card.Body>
-            {sport && <p className="mb-2">Sport: {sport}</p>}
-            {category && <p className="mb-2">Category: {category}</p>}
-            {line && <p className="mb-2">Line: {line}</p>}
-            {typeOfLine && <p className="mb-2">Type of Line: {typeOfLine}</p>}
-            {position && <p className="mb-2">Position: {position}</p>}
             {usagePercent && (
-              <p className="mb-2">Usage Percent: {usagePercent}</p>
+              <p className="mb-2">Usage Percent: {usagePercent}%</p>
             )}
-            {minutes && <p className="mb-2">Minutes: {minutes}</p>}
-            {minutesPercentage && (
-              <p className="mb-2">Minutes Percentage: {minutesPercentage}</p>
+
+            {typeOfLine && <p className="mb-2">Type of Line: {typeOfLine}</p>}
+
+            {minutes && minutesPercentage && (
+              <p className="mb-2">
+                Minutes: {minutes} ({minutesPercentage})
+              </p>
             )}
-            {projection && <p className="mb-2">Projection: {projection}</p>}
+
             {dvaPositionDefense && (
-              <p className="mb-2">DVA Position Defense: {dvaPositionDefense}</p>
+              <p className="mb-2">Up against: {dvaPositionDefense}</p>
             )}
-            <p>Source: {source}</p>
             <Button className="cardButton" variant="primary">
               Add to picks
             </Button>
