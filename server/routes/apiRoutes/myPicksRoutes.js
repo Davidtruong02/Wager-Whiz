c1ffc4504cd7b059caedd477f1d2d11e7d38f786
David Truong <Davidtruong134@gmail.com>
2024-05-09 19:46:15 -0500

    still working on branch

--- server/routes/apiRoutes/myPicksRoutes.js
+++ server/routes/apiRoutes/myPicksRoutes.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const myPicks = require("../../models/myPicks");
+const { PlayerData } = require("../../models");
 
-router.post("/myPicks", async (req, res) => {
-  try {
-    const sport = req.params.sport;
-    const myPicks = await myPicks.find({ sport });
-    res.json(myPicks);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error fetching player data");
-  }
+
+router.post("/", async (req, res) => {
+  const {username, ...playerData} = req.body;
+  
 });
 
 module.exports = router;
